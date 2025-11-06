9a20b816c9a8cf17148552fb6c188336ab96704d
Yacine Badiss <yacine.badiss@gmail.com>
2025-11-06 17:42:23 +0100

    feat: split nginx configs

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,15 +6,15 @@ echo "$SSH_OPTIONS"
 timestamp=$(date +%s)
 
 # Deploy to root of badyass.xyz (home directory)
-scp $SSH_OPTIONS root@167.71.143.97:/etc/nginx/sites-available/default nginx-config-${timestamp}
-scp $SSH_OPTIONS nginx-config root@167.71.143.97:/etc/nginx/sites-available/default
+scp $SSH_OPTIONS root@167.71.143.97:/etc/nginx/sites-available/badyass nginx-config-${timestamp}
+scp $SSH_OPTIONS nginx-config root@167.71.143.97:/etc/nginx/sites-available/badyass
 
 # Test nginx config. Make sure to reset the default config file after testing if we failed.
 ssh $SSH_OPTIONS root@167.71.143.97 "nginx -t"
 
 if [ $? -ne 0 ]; then
     echo "Nginx config test failed. Rolling back to previous config."
-    scp $SSH_OPTIONS nginx-config-${timestamp} root@167.71.143.97:/etc/nginx/sites-available/default
+    scp $SSH_OPTIONS nginx-config-${timestamp} root@167.71.143.97:/etc/nginx/sites-available/badyass
     exit 1
 fi
 
